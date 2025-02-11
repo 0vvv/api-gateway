16417efb13cd0b5ee196363decd03e436d1b8386
0vvv <18301883265@163.com>
2025-02-11 16:22:16 +0800

    装饰者response

--- src/main/java/com/lin/apigateway/CustomGlobalFilter.java
+++ src/main/java/com/lin/apigateway/CustomGlobalFilter.java
@@ -3,17 +3,26 @@ package com.lin.apigateway;
 
 import com.lin.apiclientsdk.utils.SignUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -45,7 +54,7 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         String nonce = headers.getFirst("nonce");
         String timestamp = headers.getFirst("timestamp");
         String sign = headers.getFirst("sign");
-        String body = headers.getFirst("body");
+        String body = headers.getFirst("bodyq");
         if (!accessKey.equals("lin")) {
             return handleNoAuth(response);
         }
@@ -53,7 +62,7 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
             return handleNoAuth(response);
         }
         Long currentTime = System.currentTimeMillis() / 1000;
-        Long FIVE_MINITUES = 60 * 5L;
+        final Long FIVE_MINITUES = 60 * 5L;
         if (currentTime - Long.parseLong(timestamp) >= FIVE_MINITUES) {
             return handleNoAuth(response);
         }
@@ -61,13 +70,85 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         if (!sign.equals(serverSign)) {
             return handleNoAuth(response);
         }
-        // 判断请求的模拟接口是否存在，从数据库中查询（但是该项目没有涉及到数据库的方法，这步骤不适合放在网关项目）
+        // 判断请求的模拟接口是否存在
+        // todo 从数据库中查询（但是该项目没有涉及到数据库的方法，这步骤不适合放在网关项目）
 
-        return chain.filter(exchange);
+        // 转发请求，调用模拟接口
+        // Mono<Void> filter = chain.filter(exchange);
+        return handleResponse(exchange, chain); // 确保按顺序执行，不被异步操作影响
+        // 执行完上一步的接口再输出响应日志
+    }
+
+    public Mono<Void> handleResponse(ServerWebExchange exchange, GatewayFilterChain chain) {
+        try {
+            ServerHttpResponse originalResponse = exchange.getResponse();
+            // 缓存数据
+            DataBufferFactory bufferFactory = originalResponse.bufferFactory();
+            // 获取响应码
+            HttpStatus statusCode = (HttpStatus) originalResponse.getStatusCode();
+            if (statusCode != HttpStatus.OK) {
+                return chain.filter(exchange); //降级处理返回数据
+            }
+            // 装饰response
+            ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+                @Override
+                public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                    // writeWith往返回值里面写数据
+                    if (body instanceof Flux) {
+                        // body是一个Flux实例
+                        Flux<? extends DataBuffer> fluxBody = Flux.from(body);
+                        return super.writeWith(fluxBody.buffer().map(dataBuffers -> {
+                            // 合并多个流集合，解决返回体分段传输
+                            DataBufferFactory dataBufferFactory = new DefaultDataBufferFactory();
+                            DataBuffer buff = dataBufferFactory.join(dataBuffers);
+                            byte[] content = new byte[buff.readableByteCount()];
+                            buff.read(content);
+                            DataBufferUtils.release(buff);//释放掉内存
+                            //排除Excel导出，不是application/json不打印。若请求是上传图片则在最上面判断。
+                            MediaType contentType = originalResponse.getHeaders().getContentType();
+                            if (!MediaType.APPLICATION_JSON.isCompatibleWith(contentType)) {
+                                return bufferFactory.wrap(content);
+                            }
+                            // 构建返回日志
+                            String joinData = new String(content);
+                            // 接口调用成功，接口调用次数+1
+//                            // todo 在api-backend项目中写过invokeCount，这里调用
+//                            if (response.getStatusCode() == HttpStatus.OK) {
+//
+//                            }
+//                                    // 调用失败，返回一个规范的错误
+//                            else {
+//                                return handleInvokeError(response);
+//                            }
+                            List<Object> rspArgs = new ArrayList<>();
+                            rspArgs.add(originalResponse.getStatusCode().value());
+                            rspArgs.add(exchange.getRequest().getURI());
+                            rspArgs.add(joinData);
+                            log.info("<-- {} {}\n{}", rspArgs.toArray());
+                            getDelegate().getHeaders().setContentLength(joinData.getBytes().length);
+                            return bufferFactory.wrap(joinData.getBytes());
+                        }));
+                    } else {
+                        log.error("<-- {} 响应code异常", getStatusCode());
+                    }
+                    return super.writeWith(body);
+                }
+            };
+            // 设置response对象为装饰后的对象
+            return chain.filter(exchange.mutate().response(decoratedResponse).build());
+        } catch (Exception e) {
+            log.error("gateway log exception.\n" + e);
+            return chain.filter(exchange);
+        }
     }
 
     public Mono<Void> handleNoAuth(ServerHttpResponse response) {
         response.setStatusCode(HttpStatus.FORBIDDEN);
+        return response.setComplete();
+    }
+
+    public Mono<Void> handleInvokeError(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
         return response.setComplete();
     }
 
